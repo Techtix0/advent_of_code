3fabc75d92f431b19a6e59d5c472490be2b4cf00
Techtix0 <techtix0@proton.me>
2025-12-01 16:48:30 +0100

    day01 project created

---
+++ aoc_2025/day01/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    //read line per line
+    //if L then subtract
+    //if R then add
+    //take module of result so that 100 => 0
+    //add total of 0's to result
+    //print result
+}
