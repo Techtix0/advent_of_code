258cddc58f3836372af34d17ff4e2c67202e1b80
Techtix0 <techtix0@proton.me>
2025-12-01 17:18:06 +0100

    WIP: reading input and determining L or R

--- aoc_2025/day01/src/main.rs
+++ aoc_2025/day01/src/main.rs
@@ -1,10 +1,32 @@
+use std::fs;
+
 fn main() {
     //read line per line
-    //if L then subtract
-    //if R then add
-    //take module of result so that 100 => 0
-    //add total of 0's to result
-    //print result
-    
+    let input: String = fs::read_to_string("./input.txt").expect("Error reading file.");
 
+    let mut pos: u8 = 50;
+
+    for rotation in input.split("\n") {
+        //if L then subtract
+        //if R then add
+        match &rotation.chars().nth(0).unwrap() {
+            //get number from input
+            'L' => pos -= &rotation.chars().nth().unwrap(),
+            'R' => pos += 1,
+            _ => println!("Error"),
+        }
+
+        if pos == 100 {
+            pos = 0;
+        }
+
+        println!("{pos}");
+
+        //take module of result so that 100 => 0
+        //add total of 0's to result
+        //print result
+        
+    }
+
+    
 }
