c88c06b44185b9ac1fe1b2bb6dd45d87ba958c7e
Techtix0 <techtix0@proton.me>
2025-12-02 22:57:41 +0100

    WIP

--- aoc_2025/day01/src/main.rs
+++ aoc_2025/day01/src/main.rs
@@ -5,19 +5,33 @@ fn main() {
     let input: String = fs::read_to_string("./input.txt").expect("Error reading file.");
 
     let mut pos: u8 = 50;
+    let mut i: usize = 0;
 
     for rotation in input.split("\n") {
+        i += 1;
+        println!("line: {i}");
         //if L then subtract
         //if R then add
-        match &rotation.chars().nth(0).unwrap() {
-            //get number from input
-            'L' => pos -= &rotation.chars().nth().unwrap(),
-            'R' => pos += 1,
-            _ => println!("Error"),
+        let value: &u8 = &rotation[1..].parse::<u8>().unwrap();
+
+        if value >= 100 {
+            value = value % 100;
         }
 
-        if pos == 100 {
-            pos = 0;
+        match &rotation.chars().nth(0).unwrap() {
+            //get number from input
+            'L' =>  if value < &pos {
+                pos -= value;
+            } else { 
+                pos = 100 - (value - pos);
+            },
+
+            'R' => pos += value,
+            _ => println!("Error reading value"),
+        }
+
+        if pos >= 100 {
+            pos = pos % 100;
         }
 
         println!("{pos}");
@@ -27,6 +41,5 @@ fn main() {
         //print result
         
     }
-
-    
 }
+
