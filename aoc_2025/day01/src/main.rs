f74c7cf109df7d657a7d71c5eb73b3599b06aa69
Techtix0 <techtix0@proton.me>
2025-12-03 12:59:41 +0100

    Refactoring with structs

--- aoc_2025/day01/src/main.rs
+++ aoc_2025/day01/src/main.rs
@@ -1,39 +1,39 @@
 use std::fs;
 
+struct Rotation {
+    direction: char, 
+    value: usize,
+}
+
 fn main() {
     //read line per line
     let input: String = fs::read_to_string("./input.txt").expect("Error reading file.");
 
-    let mut pos: u8 = 50;
-    let mut i: usize = 0;
+    let mut pos: usize = 50;
 
     for rotation in input.split("\n") {
-        i += 1;
-        println!("line: {i}");
-        //if L then subtract
-        //if R then add
-        let value: &u8 = &rotation[1..].parse::<u8>().unwrap();
+        let rot: Rotation = Rotation { 
+            direction: rotation.chars().nth(0).unwrap(), 
+            value: &rotation[1..].parse::<usize>().unwrap() % 100
+        };
 
-        if value >= 100 {
-            value = value % 100;
-        }
 
         match &rotation.chars().nth(0).unwrap() {
             //get number from input
-            'L' =>  if value < &pos {
-                pos -= value;
-            } else { 
-                pos = 100 - (value - pos);
+            'L' =>  if rot.value < pos {
+                pos -= rot.value;
+            } else {
+                pos = 100 - (rot.value - pos);
             },
 
-            'R' => pos += value,
+            'R' => pos += rot.value,
             _ => println!("Error reading value"),
         }
 
         if pos >= 100 {
             pos = pos % 100;
         }
-
+        
         println!("{pos}");
 
         //take module of result so that 100 => 0
